109d5e28837fc1e8f45a4972721f693f21f1bb8a
Gary <102333968+MrRustyKMaynard@users.noreply.github.com>
2023-04-11 18:36:33 +0100

    createdBlips table
    modified the loop that creates the blips to first check if a blip with the same coordinates already exists in the createdBlips table. If it does, the loop skips that blip and moves on to the next one. If it doesn't, the blip is created as before, and its coordinates are added to the createdBlips table so that we know not to create another blip with the same coordinate
    
    Updated Config.lua to suit

--- client.lua
+++ client.lua
@@ -1,14 +1,22 @@
 
+local createdBlips = {}
+
 -- Loop through each blip in the configuration and create it
 for i, blipData in ipairs(Config.Blips) do
-    local blip = AddBlipForCoord(blipData.coords.x, blipData.coords.y, blipData.coords.z)
-    SetBlipSprite(blip, blipData.sprite)
-    SetBlipDisplay(blip, blipData.display)
-    SetBlipScale(blip, blipData.scale)
-    SetBlipColour(blip, blipData.color)
-    SetBlipAsShortRange(blip, blipData.shortRange)
+    -- Check if the blip has already been created
+    if not createdBlips[blipData.id] then
+        local blip = AddBlipForCoord(blipData.coords.x, blipData.coords.y, blipData.coords.z)
+        SetBlipSprite(blip, blipData.sprite)
+        SetBlipDisplay(blip, blipData.display)
+        SetBlipScale(blip, blipData.scale)
+        SetBlipColour(blip, blipData.color)
+        SetBlipAsShortRange(blip, blipData.shortRange)
 
-    BeginTextCommandSetBlipName("STRING")
-    AddTextComponentString(blipData.name)
-    EndTextCommandSetBlipName(blip)
+        BeginTextCommandSetBlipName("STRING")
+        AddTextComponentString(blipData.name)
+        EndTextCommandSetBlipName(blip)
+
+        -- Mark the blip as created
+        createdBlips[blipData.id] = true
+    end
 end
--- config.lua
+++ config.lua
@@ -2,7 +2,8 @@ Config = {}
 
 --Define the blips--Edit in your Blips below
 Config.Blips = {
-    {
+    {   
+        id = 1,
         name = "Airport",
         sprite = 90,
         color = 3,
@@ -11,7 +12,8 @@ Config.Blips = {
         shortRange = true,
         coords = vector3(2149.59, 4817.6, 41.268)
     },
-    {
+    {   
+        id = 2,
         name = "Airport",
         sprite = 90,
         color = 3,
@@ -20,7 +22,8 @@ Config.Blips = {
         shortRange = true,
         coords = vector3(-1214.46, -3022.72, 13.94)
     },
-    {
+    {   
+        id = 3,
         name = "Airport",
         sprite = 90,
         color = 3,
@@ -29,7 +32,8 @@ Config.Blips = {
         shortRange = true,
         coords = vector3(4271.85, -4576.44, 4.18)
     },
-    {
+    {   
+        id = 4,
         name = "Bank Truck Heist",
         sprite = 810,
         color = 1,
@@ -38,7 +42,8 @@ Config.Blips = {
         shortRange = true,
         coords = vector3(960.94, -215.22, 76.26)
     },
-    {
+    {   
+        id = 5,
         name = "Fire Station",
         sprite = 60,
         color = 1,
@@ -48,6 +53,7 @@ Config.Blips = {
         coords = vector3(1203.78, -1457.99, 34.96)
     },
     {
+        id = 6,
         name = "Hospital",
         sprite = 61,
         color = 74,
@@ -57,6 +63,7 @@ Config.Blips = {
         coords = vector3(-250.43, 6327.6, 32.43)
     },
     {
+        id = 7,
         name = "Rally Track",
         sprite = 379,
         color = 38,
@@ -66,6 +73,7 @@ Config.Blips = {
         coords = vector3(2260.5, 1972.6, 122.16)
     },
     {
+        id = 8,
         name = "Strip Club",
         sprite = 121,
         color = 26,
@@ -75,6 +83,7 @@ Config.Blips = {
         coords = vector3(132.7, -1301.3, 29.23)
     },
     {
+        id = 9,
         name = "Black Market",
         sprite = 378,
         color = 1,
@@ -84,6 +93,7 @@ Config.Blips = {
         coords = vector3(4441.86, -4445.92, 7.24)
     },
     {
+        id = 10,
         name = "Drag Strip",
         sprite = 38,
         color = 44,
@@ -93,6 +103,7 @@ Config.Blips = {
         coords = vector3(1382.66, 3161.24, 40.41)
     },
     {
+        id = 11,
         name = "Gas Station",
         sprite = 361,
         color = 4,
